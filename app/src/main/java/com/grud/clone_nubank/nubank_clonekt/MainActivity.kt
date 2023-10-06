c4046694600ca6f6997a1e2614e3c224d42f6e09
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2023-10-06 13:07:29 -0300

    Start

---
+++ app/src/main/java/com/grud/clone_nubank/nubank_clonekt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.grud.clone_nubank.nubank_clonekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
